9ee68b67de9724dde76fb22fad4045616af20d5a
MatthewBlake <mzb1998@gmail.com>
2020-11-21 23:57:37 +0000

    Started dynamically updating pie chart from json file
    Labels for pie chart are now taken from portfolio.json - adding values from portfolio.json to be done

--- dividend_tracker_react/src/App.js
+++ dividend_tracker_react/src/App.js
@@ -26,27 +26,46 @@ const Holdings=()=>{
   )
 }
 
+// positions[0].valueOfHoldingInEuro
+// positions[1].valueOfHoldingInEuro
+// positions[2].valueOfHoldingInEuro
+// positions[3].valueOfHoldingInEuro
+// positions[4].valueOfHoldingInEuro
+// positions[5].valueOfHoldingInEuro
+// positions[6].valueOfHoldingInEuro
+// positions[7].valueOfHoldingInEuro
+// positions[8].valueOfHoldingInEuro
+// positions[9].valueOfHoldingInEuro
+// positions[10].valueOfHoldingInEuro
+// positions[11].valueOfHoldingInEuro
+// positions[12].valueOfHoldingInEuro
+// positions[13].valueOfHoldingInEuro
+// positions[14].valueOfHoldingInEuro
+// positions[15].valueOfHoldingInEuro
+
+const data=[
+        {title: positions[0].ticker, value: 100, color: 'blue'},
+        {title: positions[1].ticker, value: 100, color: '#BFFCC6'},
+        {title: positions[2].ticker, value: 100, color: '#FF9CEE'},
+        {title: positions[3].ticker, value: 100, color: '#C5A3FF'},
+        {title: positions[4].ticker, value: 100, color: '#C4FAF8'},
+        {title: positions[5].ticker, value: 100, color: '#DBFFD6'},
+        {title: positions[6].ticker, value: 100, color: '#FFB5E8'},
+        {title: positions[7].ticker, value: 100, color: '#FFC9DE'},
+        {title: positions[8].ticker, value: 100, color: '#FFABAB'},
+        {title: positions[9].ticker, value: 100, color: '#AFF8D8'},
+        {title: positions[10].ticker, value: 100, color: '#85E3FF'},
+        {title: positions[11].ticker, value: 100, color: '#FFBEBC'},
+        {title: positions[12].ticker, value: 100, color: '#ECD4FF'},
+        {title: positions[13].ticker, value: 100, color: '#FFF5BA'},
+        {title: positions[14].ticker, value: 100, color: '#B28DFF'},
+        {title: positions[15].ticker, value: 100, color: '#6EB5FF'},
+      ];
+
 const Chart=()=>{
   return (
     <React.Fragment>
-      <PieChart style={{backgroundColor: '#404953'}} radius={20} labelPosition={112} animate data={[
-        {title: 'ABBV', value: 147.33, color: '#DCD3FF'},
-        {title: 'BMO', value: 176.97, color: '#BFFCC6'},
-        {title: 'BNS', value: 149.87, color: '#FF9CEE'},
-        {title: 'CL', value: 142.71, color: '#C5A3FF'},
-        {title: 'CSCO', value: 98.13, color: '#C4FAF8'},
-        {title: 'JNJ', value: 166.07, color: '#DBFFD6'},
-        {title: 'JPM', value: 114.82, color: '#FFB5E8'},
-        {title: 'KO', value: 133.10, color: '#FFC9DE'},
-        {title: 'LEG', value: 137.87, color: '#FFABAB'},
-        {title: 'MMM', value: 144.54, color: '#AFF8D8'},
-        {title: 'MRK', value: 106, color: '#85E3FF'},
-        {title: 'PEP', value: 120.91, color: '#FFBEBC'},
-        {title: 'O', value: 154.81, color: '#ECD4FF'},
-        {title: 'STOR', value: 138.60, color: '#FFF5BA'},
-        {title: 'T', value: 205.48, color: '#B28DFF'},
-        {title: 'VZ', value: 137.07, color: '#6EB5FF'},
-      ]}/>
+      <PieChart style={{backgroundColor: '#404953'}} radius={20} labelPosition={112} animate data={data}/>
     </React.Fragment>
   )
 }
